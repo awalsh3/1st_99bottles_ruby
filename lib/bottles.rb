f30cc577819a241718b07e042e941a373ae13c4c
Annie Walsh <annie@anyonething.co.uk>
2023-04-11 17:44:03 +0100

    Adds test_verse_2

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,6 +1,16 @@
 class Bottles
   def verse(amount)
+
+  if amount == 2
+    "#{amount} bottles of beer on the wall, #{amount} bottles of beer.
+    Take one down and pass it around, #{amount - 1} bottle of beer on the wall."
+  elsif amount <= 1
+    "#{amount} bottles of beer on the wall, #{amount} bottles of beer.
+    Take one down and pass it around, no more bottles of beer on the wall."
+  elsif amount > 1
     "#{amount} bottles of beer on the wall, #{amount} bottles of beer.
     Take one down and pass it around, #{amount - 1} bottles of beer on the wall."
+  end
+
   end
 end
