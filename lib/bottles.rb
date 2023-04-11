8e26517354c1bf2dccd5b5f7a9eac9555b580f65
Annie Walsh <annie@anyonething.co.uk>
2023-04-11 17:20:28 +0100

    First test

---
+++ lib/bottles.rb
@@ -0,0 +1,6 @@
+class Bottles
+  def verse(amount)
+    "#{amount} bottles of beer on the wall, #{amount} bottles of beer.
+    Take one down and pass it around, #{amount - 1} bottles of beer on the wall."
+  end
+end
